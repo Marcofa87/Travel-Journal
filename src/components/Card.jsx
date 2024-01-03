a6776c61780ce676cd4afec8e7c8879c752fd6c0
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-01-03 18:21:03 +0100

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <>
+      <div className="container">
+        <img src={props.imageUrl} />
+        <div className="container-head">
+          <div className="container-body">
+            <img src="../../images/location-dot-solid.svg" alt="" />
+            <h4>{props.location}</h4>
+            <a href={props.googleMapsUrl}>View on Google Maps</a>
+          </div>
+          <div className="container-foot">
+            <h2>{props.title}</h2>
+            <p className="bold">
+              {props.startDate} - {props.endDate}
+            </p>
+            <p className="description">{props.description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="nav">
+        <img src="../../images/earth.png" />
+        <h3>My travel journal</h3>
+      </div>
+    </>
+  );
+}
