8e446b3a232696fcfdfa19350f2b5c92a607033d
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-21 14:45:15 +0100

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar() {
   return (
     <>
       <div className="nav">
+        <p>ciao</p>
         <img src="../../public/images/earth.png" />
         <h3>My travel journal</h3>
       </div>
